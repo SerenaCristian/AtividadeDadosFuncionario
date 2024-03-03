1c055d28ab7dd0381c82fbff24a214596315de46
Cristian Serena <devcristian02@gmail.com>
2024-03-03 01:45:06 -0300

    Adicionada a Classe e os metodos de aumento de salario e a condiçãi de validação se ele é elegivel a receber aumento

--- DadosFuncionario.cs
+++ DadosFuncionario.cs
@@ -12,37 +12,35 @@ namespace DadosFuncionario
             double desconto;
             double salarioliquido;
             double promocao;
+            double valorPromocao;
             string dot = new string('.', 50);
+
+            Funcionario funcionario = new Funcionario();
 
             Console.WriteLine("Sistema Dados Funcionário");
             Console.WriteLine(dot);
 
             Console.Write("\nDigite o Nome do Funcionário: ");
-            nome = Console.ReadLine();
+            funcionario.Nome = Console.ReadLine();
 
             Console.Write("\nDigite o Valor do Salário do Funcionário: ");
-            salariobruto = double.Parse(Console.ReadLine());
+            funcionario.salariobruto = double.Parse(Console.ReadLine());
 
             Console.Write("\nO funcionário é elegível para receber aumento? Digite 1 para sim, 2 para não: ");
             int opcao = int.Parse(Console.ReadLine());
 
             Console.WriteLine(dot);
 
-            // Calcula o desconto dos impostos no salário
-            desconto = salariobruto * imposto;
-            salarioliquido = salariobruto - desconto;
-
-            // Aqui será implementada a lógica do aumento
-            promocao = 0.10;
-            double aumento = salariobruto * promocao;
-
             if (opcao == 1)
             {
+                funcionario.CalcularPromocao(out valorPromocao, out promocao);
                 Console.WriteLine("\nParabéns! Você ganhou 10% de aumento.");
-                Console.WriteLine($"\nSeu novo salário é de: {aumento + salariobruto}");
-                Console.WriteLine($"\nNome do Funcionário: {nome}");
-                Console.WriteLine($"\nO desconto será de: {(desconto + aumento).ToString("F2")}");
-                Console.WriteLine($"\nO salário líquido é de: {(salarioliquido + aumento).ToString("F2")}");
+                Console.WriteLine($"\nSeu novo salário é de: {valorPromocao + funcionario.salariobruto} ");
+                desconto = funcionario.CalculaImposto(funcionario.salariobruto, imposto);
+                Console.WriteLine($"\nNome do Funcionário: {funcionario.Nome}");
+                Console.WriteLine($"\nO desconto será de: {desconto}");
+
+                Console.WriteLine($"\nSalario Liquido é de: {(valorPromocao + funcionario.salariobruto) - desconto} ");
 
                 Console.WriteLine("\n* ATENÇÃO: TOTAL DE DESCONTO É 34%");
                 Console.WriteLine(dot);
@@ -54,4 +52,5 @@ namespace DadosFuncionario
         }
     }
 }
+
 
--- Funcionario.cs
+++ Funcionario.cs
@@ -1,9 +1,28 @@
 ﻿using System;
 
-
 namespace DadosFuncionario
 {
     public class Funcionario
     {
+        public string Nome;
+        public double salariobruto;
+        public double imposto = 0.34;
+        public double desconto;
+        public double salarioliquido;
+        public double promocao;
+        public double valorimposto;
+        public double valorPromocao;
+
+        public double CalculaImposto(double salariobruto, double imposto)
+        {
+            double valorimposto = salariobruto * imposto;
+            return valorimposto;
+        }
+
+        public void CalcularPromocao(out double valorPromocao, out double promocao)
+        {
+            promocao = 0.10;
+            valorPromocao = salariobruto * promocao;
+        }
     }
 }
